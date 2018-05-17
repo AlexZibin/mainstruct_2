87acd865b22d16460df7f762211a0b5992e13ded
Alexander Zibin <37593964+AlexZibin@users.noreply.github.com>
2018-05-17 12:21:47 +0300

    Update mainstruct_2.ino

--- mainstruct_2.ino
+++ mainstruct_2.ino
@@ -92,13 +92,13 @@ void loop () {
     */
 
     if (button.shortPress()) {
-      Serial.println (++numPresses);
-      Serial.println ("intro->nextMode();");
-      intro->nextMode();
+        Serial.println (++numPresses);
+        Serial.println ("intro->nextMode();");
+        intro->nextMode();
     }
     if (button.longPress()) {
-      Serial.println ("intro->applyMode (fColorDemo1);");
-      intro->applyMode (fColorDemo1);
+        Serial.println ("intro->applyMode (fColorDemo1);");
+        intro->applyMode (fColorDemo1);
     }
     //backlightLEDs ();
     LEDS.show ();
@@ -142,112 +142,119 @@ void readEEPROM (void) {}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 returnValue fColorDemo2 (long currentCallNumber) {
-  static unsigned long millisAtStart;
-  
-  if (currentCallNumber == 0) {
+    static unsigned long millisAtStart;
+    const long playTimeMs = 18000;
+
+    if (currentCallNumber == 0) {
       millisAtStart = millis ();
-      for (int led = 0; led < numLEDs; led++) { // Don't erase digits
+      for (int led = 0; led < numLEDs; led++) { // Don't erase starting LEDs (digits 3-6-9-12)
           findLED(led)->r = 0;
           findLED(led)->g = 0;
           findLED(led)->b = 0;
       }
-  }
-  
-  unsigned long deltaT = millis () - millisAtStart;
-  unsigned long timeStep = 5;
-  int direction = -1;
-  float wavelen = 10.0;
+    }
 
-  if (deltaT > 20000) { LEDS.clear (); return returnValue::NEXT; };
+    unsigned long deltaT = millis () - millisAtStart;
+    unsigned long timeStep = 5;
+    int direction = -1;
+    float wavelen = 10.0;
 
-  static float ledBrightness;
-  byte r, g, b;
-  Wheel ((deltaT/15)%384, r, g, b);
-  for (int led = 0; led < numLEDs; led++) {
-      uint8_t firstBrightness = sine8_0 (static_cast<uint8_t>(deltaT/timeStep/(1-deltaT/48000.0)-direction*led*wavelen*(1+deltaT/22000.0))%256);
-     
-      float brt;
-      if (deltaT <= 3000.0) ledBrightness = deltaT/3000.0;
-      const float shade = 5;
-      if (led < shade) {      
+    if (deltaT > playTimeMs) { LEDS.clear (); return returnValue::NEXT; };
+
+    static float ledBrightness;
+    byte r, g, b;
+    Wheel ((deltaT/15)%384, r, g, b);
+    for (int led = 0; led < numLEDs; led++) {
+        uint8_t firstBrightness = sine8_0 (static_cast<uint8_t>(deltaT/timeStep/(1-deltaT/(playTimeMs*2.1))-direction*led*wavelen*(1+deltaT/(playTimeMs+4000.0)))%256);
+        
+        // dimming at the beginning of demo and in the end:
+        const float dimmingTimeMs = 3000.0;
+        if (deltaT <= dimmingTimeMs) ledBrightness = deltaT/dimmingTimeMs;
+        if (deltaT >= playTimeMs-dimmingTimeMs) ledBrightness = (playTimeMs-deltaT)/dimmingTimeMs;
+        
+        float brt;
+        const float shade = 5;
+        if (led < shade) {      // gimming LEDs near 12 o'clock
           brt = NeoPixel_gamma8(firstBrightness*(led/shade)*ledBrightness)/128.0;
           findLED(led)->r = brt*r;
           findLED(led)->g = brt*g;
           findLED(led)->b = brt*b;
-      } else if (led >= numLEDs - shade) {      
+        } else if (led >= numLEDs - shade) {      
           brt = NeoPixel_gamma8(firstBrightness*((numLEDs-led)/shade)*ledBrightness)/128.0;
           findLED(led)->r = brt*r;
           findLED(led)->g = brt*g;
           findLED(led)->b = brt*b;
-      } else {
+        } else {
           brt = NeoPixel_gamma8(firstBrightness*ledBrightness)/128.0;
           findLED(led)->r = brt*r;
           findLED(led)->g = brt*g;
           findLED(led)->b = brt*b;
-      }
-  }
+        }
+    }
 
-  return returnValue::CONTINUE;
+    return returnValue::CONTINUE;
 }
 
 returnValue fColorDemo1 (long currentCallNumber) {
-  static unsigned long millisAtStart;
-  
-  //Serial.println ("Mode: fColorDemo1");
-  //Serial.println (currentCallNumber);
-  
-  if (currentCallNumber == 0) {
+    static unsigned long millisAtStart;
+    const long playTimeMs = 18000;
+
+    //Serial.println ("Mode: fColorDemo1");
+    //Serial.println (currentCallNumber);
+
+    if (currentCallNumber == 0) {
       millisAtStart = millis ();
       LEDS.clear(true);
-  }
-  
-  unsigned long deltaT = millis () - millisAtStart;
-  unsigned long timeStep = 5;
-  int direction = 1;
-  float wavelen = 10.0;
+    }
 
-  /*if (deltaT >  3000) { wavelen = 20; timeStep =  4; };
-  if (deltaT >  6000) { wavelen = 20; timeStep =  3; };
-  if (deltaT >  9000) { wavelen = 20; timeStep =  2; };
-  if (deltaT > 12000) { wavelen = 20; timeStep =  1; };
-  if (deltaT > 15000) { wavelen = 20; direction = -1; };*/
-  if (deltaT > 20000) { /*LEDS.clear (); */return returnValue::NEXT; };
+    unsigned long deltaT = millis () - millisAtStart;
+    unsigned long timeStep = 5;
+    int direction = 1;
+    float wavelen = 10.0;
 
-  //Serial.print ("deltaT: ");   Serial.println (deltaT);
+    /*if (deltaT >  3000) { wavelen = 20; timeStep =  4; };
+    if (deltaT >  6000) { wavelen = 20; timeStep =  3; };
+    if (deltaT >  9000) { wavelen = 20; timeStep =  2; };
+    if (deltaT > 12000) { wavelen = 20; timeStep =  1; };
+    if (deltaT > 15000) { wavelen = 20; direction = -1; };*/
+    if (deltaT > playTimeMs) { /*LEDS.clear (); */return returnValue::NEXT; };
 
-  static float ledBrightness;
-  for (int led = 0; led < numLEDs; led++) {
-//     uint8_t firstBrightness = NeoPixel_gamma8(NeoPixel_sine8(static_cast<uint8_t>(deltaT/timeStep-direction*led*wavelen)%256));
-//     uint8_t firstBrightness = sine8_0 ((deltaT/timeStep-direction*led*wavelen)%256);
-     uint8_t firstBrightness = sine8_0 (static_cast<uint8_t>(deltaT/timeStep/(1-deltaT/48000.0)-direction*led*wavelen*(1+deltaT/22000.0))%256);
-     uint8_t firstBrightnessB = sine8_0 (static_cast<uint8_t>(deltaT/timeStep/(1-deltaT/58000.0)+direction*led*wavelen*(1-deltaT/76000.0))%256);
-//     uint8_t firstBrightnessB = sine8_0 (static_cast<uint8_t>(deltaT/timeStep/(1-deltaT/20000.0)+direction*led*wavelen*(1-deltaT/36000.0))%256);
-     
-     if (deltaT <= 3000.0) ledBrightness = deltaT/3000.0;
-     const float shade = 5;
-     if (led < shade) {      
-          findLED(led)->b = NeoPixel_gamma8(firstBrightness*(led/shade)*ledBrightness);
-          //findLED(led)->b = NeoPixel_gamma8(firstBrightnessB*(led/shade)/4);
-     } else if (led >= numLEDs - shade) {      
-          findLED(led)->b = NeoPixel_gamma8(firstBrightness*((numLEDs-led)/shade)*ledBrightness);
-          //findLED(led)->b = NeoPixel_gamma8(firstBrightnessB*((numLEDs-led)/shade)/4);
-     } else
-          findLED(led)->b = NeoPixel_gamma8(firstBrightness*ledBrightness);
-          //findLED(led)->b = NeoPixel_gamma8(firstBrightnessB/4);
-     //Serial.print (led); Serial.print (": "); Serial.println (firstBrightness); 
-  }
+    //Serial.print ("deltaT: ");   Serial.println (deltaT);
 
-  byte r, g, b;
-  static float ledBrightness2;
-  if (deltaT <= 16000.0) ledBrightness2 = deltaT/16000.0;
-  Wheel ((deltaT/15)%384, r, g, b);
-  for (int i = 0; i < startingLEDs; i++) {
-      _leds[i].r = r*2*ledBrightness2;
-      _leds[i].g = g*2*ledBrightness2;
-      _leds[i].b = b*2*ledBrightness2;
-  }
+    static float ledBrightness;
+    for (int led = 0; led < numLEDs; led++) {
+        uint8_t firstBrightness = sine8_0 (static_cast<uint8_t>(deltaT/timeStep/(1-deltaT/(playTimeMs*3.1))-direction*led*wavelen*(1+deltaT/(playTimeMs*1.3)))%256);
+        //uint8_t firstBrightnessB = sine8_0 (static_cast<uint8_t>(deltaT/timeStep/(1-deltaT/(playTimeMs*4.1))+direction*led*wavelen*(1-deltaT/76000.0))%256);
 
-  return returnValue::CONTINUE;
+        // dimming at the beginning of demo and in the end:
+        const float dimmingTimeMs = 3000.0;
+        if (deltaT <= dimmingTimeMs) ledBrightness = deltaT/dimmingTimeMs;
+        if (deltaT >= playTimeMs-dimmingTimeMs) ledBrightness = (playTimeMs-deltaT)/dimmingTimeMs;
+
+        const float shade = 5;
+        if (led < shade) {      
+            findLED(led)->b = NeoPixel_gamma8(firstBrightness*(led/shade)*ledBrightness);
+            //findLED(led)->b = NeoPixel_gamma8(firstBrightnessB*(led/shade)/4);
+        } else if (led >= numLEDs - shade) {      
+            findLED(led)->b = NeoPixel_gamma8(firstBrightness*((numLEDs-led)/shade)*ledBrightness);
+            //findLED(led)->b = NeoPixel_gamma8(firstBrightnessB*((numLEDs-led)/shade)/4);
+        } else
+            findLED(led)->b = NeoPixel_gamma8(firstBrightness*ledBrightness);
+            //findLED(led)->b = NeoPixel_gamma8(firstBrightnessB/4);
+            //Serial.print (led); Serial.print (": "); Serial.println (firstBrightness); 
+    }
+
+    byte r, g, b;
+    static float ledBrightness2;
+    if (deltaT <= 16000.0) ledBrightness2 = deltaT/16000.0;
+    Wheel ((deltaT/15)%384, r, g, b);
+    for (int i = 0; i < startingLEDs; i++) {
+          _leds[i].r = r*2*ledBrightness2;
+          _leds[i].g = g*2*ledBrightness2;
+          _leds[i].b = b*2*ledBrightness2;
+    }
+
+    return returnValue::CONTINUE;
 }
 
 returnValue fColorDemo4 (long currentCallNumber) {
